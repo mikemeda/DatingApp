3f67c019b6ad8e0049c7e5e69e1560aa348708c4
Michael Alemayehu <michaelalemayehu21@gmail.com>
2020-05-13 17:48:05 -0700

    Pre-Publishing

--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -15,7 +15,7 @@ namespace DatingApp.API.Models
         public bool IsMain { get; set; }
         public string PublicID { get; set; }
 
-        public User User { get; set; }
+        public virtual User User { get; set; }
 
         public int UserId { get; set; }
 
